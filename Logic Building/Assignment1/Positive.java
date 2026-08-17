d1a63434e60ae1a24f125bbcdc4ea65244e4d01e
Vaishnavi Baraskar <vaishnavi.baraskar.cmaug26@gmail.com>
2026-08-17 22:21:56 +0530

    Add files via upload

---
+++ Logic Building/Assignment1/Positive.java
@@ -0,0 +1,10 @@
+public class Positive{
+public static void main(String args[]){
+int num=12;
+if (num>0)
+System.out.println("Number is positive ");
+else
+System.out.println("Number is Not Positive ");
+
+}
+}
